de4b47f715d6f7034bae62f46ee46c07fed5d375
Warot Tangswanit <warot@khroton.com>
2014-12-11 12:04:27 +0700

    Add dpToPxFloat method in PixelHelper.

--- KuNong/src/main/java/kunong/android/library/helper/PixelHelper.java
+++ KuNong/src/main/java/kunong/android/library/helper/PixelHelper.java
@@ -7,10 +7,16 @@ import android.util.TypedValue;
 public class PixelHelper {
 
 	public static int dpToPx(Context context, float value) {
-		Resources r = context.getResources();
-		float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, r.getDisplayMetrics());
+		float px = dpToPxFloat(context, value);
 
 		return (int) (px + 0.5);
 	}
+
+    public static float dpToPxFloat(Context context, float value) {
+        Resources r = context.getResources();
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, r.getDisplayMetrics());
+
+        return px;
+    }
 
 }
