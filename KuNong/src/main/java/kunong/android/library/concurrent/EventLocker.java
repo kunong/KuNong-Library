4d7fe401f6a9a1deee45ff248a20f9ec3576c010
Warot Tangswanit <ong.mysteriousman@gmail.com>
2014-12-15 01:23:59 +0700

    Add EventLocker class.

---
+++ KuNong/src/main/java/kunong/android/library/concurrent/EventLocker.java
@@ -0,0 +1,108 @@
+package kunong.android.library.concurrent;
+
+import android.util.SparseArray;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by kunong on 12/14/14 AD.
+ */
+public final class EventLocker {
+
+    private HashMap<String, Boolean> mKeys = new HashMap<>();
+    private SparseArray<Runnable> mRunnables = new SparseArray<>();
+    private List<Runnable> mRunnableQueue = new ArrayList<>();
+    private boolean mIsChecking = false;
+
+    public synchronized void lock(String key) {
+        mKeys.put(key, true);
+    }
+
+    public synchronized void unlock(String key) {
+        mKeys.remove(key);
+
+        if (isKeyEmpty()) {
+            check();
+        }
+    }
+
+    public void run(Runnable runnable) {
+        run(runnable.hashCode(), runnable);
+    }
+
+    public void run(String runKey, Runnable runnable) {
+        run(runKey.hashCode(), runnable);
+    }
+
+    private synchronized void run(int runCode, Runnable runnable) {
+        remove(runCode);
+
+        if (isKeyEmpty()) {
+            runnable.run();
+        } else {
+            mRunnables.put(runCode, runnable);
+            mRunnableQueue.add(runnable);
+        }
+    }
+
+    public void remove(String runKey) {
+        remove(runKey.hashCode());
+    }
+
+    private synchronized void remove(int runCode) {
+        AtomicInteger index = new AtomicInteger();
+        Runnable runnable = getRunnable(runCode, index);
+
+        if (runnable != null) {
+            mRunnables.removeAt(index.get());
+            mRunnableQueue.remove(runnable);
+        }
+    }
+
+    private synchronized Runnable getRunnable(int runCode) {
+        return mRunnables.get(runCode);
+    }
+
+    private synchronized Runnable getRunnable(int runCode, AtomicInteger index) {
+        index.set(mRunnables.indexOfKey(runCode));
+
+        return index.get() >= 0 ? mRunnables.valueAt(index.get()) : null;
+    }
+
+    private synchronized boolean check() {
+        if (mIsChecking)
+            return true;
+
+        mIsChecking = true;
+
+        Async.main(() -> {
+            synchronized (this) {
+                mIsChecking = false;
+
+                if (!isKeyEmpty())
+                    return;
+
+                dispatch();
+            }
+        });
+
+        return false;
+    }
+
+    private synchronized void dispatch() {
+        for (Runnable runnable : mRunnableQueue) {
+            runnable.run();
+        }
+
+        mRunnableQueue.clear();
+        mRunnables.clear();
+    }
+
+    private boolean isKeyEmpty() {
+        return mKeys.size() == 0;
+    }
+
+}
