c436cfa21603ce35ff12e1d27b871eb1f56f4b9f
Warot Tangswanit <ong.mysteriousman@gmail.com>
2015-05-30 16:10:47 +0700

    Update retrolambda version.

--- KuNong/src/main/java/kunong/android/library/concurrent/ThreadQueue.java
+++ KuNong/src/main/java/kunong/android/library/concurrent/ThreadQueue.java
@@ -4,47 +4,48 @@ import android.os.Handler;
 import android.os.Looper;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ThreadQueue {
 
-	private Handler mHandler = new Handler(Looper.getMainLooper());
-	private ArrayList<QThread<?, ?>> qThreadList = new ArrayList<>();
-	private ThreadQueueListener mListener;
+    private Handler mHandler = new Handler(Looper.getMainLooper());
+    private List<QThread<?, ?>> qThreadList = new ArrayList<>();
+    private ThreadQueueListener mListener;
 
-	public void add(QThread<?, ?> qThread) {
-		qThreadList.add(qThread);
-	}
+    public void add(QThread<?, ?> qThread) {
+        qThreadList.add(qThread);
+    }
 
-	public void start() {
-		new Thread() {
+    public void start() {
+        new Thread() {
 
-			@Override
-			public void run() {
-				while (qThreadList.size() > 0) {
-					// Pop QThread;
-					QThread<?, ?> qThread = qThreadList.remove(0);
+            @Override
+            public void run() {
+                while (qThreadList.size() > 0) {
+                    // Pop QThread;
+                    QThread<?, ?> qThread = qThreadList.remove(0);
 
-					// Execute process in background.
-					qThread.execute();
+                    // Execute process in background.
+                    qThread.execute();
 
-					// Execute completed process in ui thread.
-					mHandler.post(qThread);
-				}
+                    // Execute completed process in ui thread.
+                    mHandler.post(qThread);
+                }
 
-				// Callback after completed all threads.
-				if (mListener != null) {
-					mHandler.post(mListener::onCompleted);
-				}
-			}
+                // Callback after completed all threads.
+                if (mListener != null) {
+                    mHandler.post(mListener::onCompleted);
+                }
+            }
 
-		}.start();
-	}
+        }.start();
+    }
 
-	public void setListener(ThreadQueueListener listener) {
-		mListener = listener;
-	}
+    public void setListener(ThreadQueueListener listener) {
+        mListener = listener;
+    }
 
-	public interface ThreadQueueListener {
-		void onCompleted();
-	}
+    public interface ThreadQueueListener {
+        void onCompleted();
+    }
 }
